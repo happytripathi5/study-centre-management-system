061e16db82e3c9fb5fc2f2873f3d219f03bffa25
utkarsh <lionel.happy2014@gmail.com>
2026-09-15 14:12:49 +0530

    Added the sping confriguration class that is really important as we had the Spring security--- now can add the student and user

---
+++ src/main/java/com/studycentre/Study_Centre/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.studycentre.Study_Centre.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/students").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+
+
+
+    }
+}
